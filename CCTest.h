ade9af1880aecde384b14ab950679452e7c28eab
chen <chenchen.hnu@gmail.com>
2015-03-20 21:04:38 -0500

    added test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Chen Chen on 3/20/15.
+//  Copyright (c) 2015 Chen Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Chen Chen on 3/20/15.
+//  Copyright (c) 2015 Chen Chen. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
