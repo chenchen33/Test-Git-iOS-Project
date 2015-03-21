dc683a5b01c0e2e1e3354c3de84110ffae680b1e
Chen Chen <Chen@Chens-iMac.local>
2015-03-20 20:46:04 -0500

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Chen Chen on 3/20/15.
+//  Copyright (c) 2015 Chen Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Chen Chen on 3/20/15.
+//  Copyright (c) 2015 Chen Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
